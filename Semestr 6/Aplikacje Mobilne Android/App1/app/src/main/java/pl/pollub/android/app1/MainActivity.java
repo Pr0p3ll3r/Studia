1471922ff112f88a0437480a568896f38fb80443
Jakub Dudek <kuba395@gmail.com>
2023-03-16 23:34:35 +0100

    Android Lab1 First App Done

--- Semestr 6/Aplikacje Mobilne Android/App1/app/src/main/java/pl/pollub/android/app1/MainActivity.java
+++ Semestr 6/Aplikacje Mobilne Android/App1/app/src/main/java/pl/pollub/android/app1/MainActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isImieEtOk;
+    private boolean isNazwiskoEtOk;
+    private boolean isLiczbaOcenEtOk;
+    private boolean isPotwierdzEtWidoczny;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +22,56 @@ public class MainActivity extends AppCompatActivity {
         EditText imieEt = findViewById(R.id.imie_et);
         imieEt.setOnFocusChangeListener((v, hasFocus) -> {
             if(hasFocus) return;
-            if("".equals(imieEt.getText().toString())){
-                imieEt.setError(getString(R.string.imie_et_err));
-                Toast.makeText(this, R.string.imie_et_err, Toast.LENGTH_LONG).show();
-            }
+            this.isImieEtOk = sprawdzEt(imieEt, getString(R.string.imie_et_err));
+            sprawdzPotwierdzBtWidoczny();
         });
+
         EditText nazwiskoEt = findViewById(R.id.nazwisko_et);
         nazwiskoEt.setOnFocusChangeListener((v, hasFocus) -> {
             if(hasFocus) return;
-            if("".equals(nazwiskoEt.getText().toString())){
-                nazwiskoEt.setError(getString(R.string.nazwisko_et_err));
-                Toast.makeText(this, R.string.nazwisko_et_err, Toast.LENGTH_LONG).show();
-            }
+            this.isNazwiskoEtOk = sprawdzEt(nazwiskoEt, getString(R.string.nazwisko_et_err));
+            sprawdzPotwierdzBtWidoczny();
         });
+
+        EditText liczbaOcenEt = findViewById(R.id.liczba_ocen_et);
+        liczbaOcenEt.setOnFocusChangeListener((v, hasFocus) -> {
+            if(hasFocus) return;
+            this.isLiczbaOcenEtOk = sprawdzLiczbeOcen(liczbaOcenEt, getString(R.string.liczba_ocen_et_err));
+            sprawdzPotwierdzBtWidoczny();
+        });
+    }
+
+    private boolean sprawdzEt(EditText editText, String info) {
+        if("".equals(editText.getText().toString())){
+            editText.setError(info);
+            Toast.makeText(this, info, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean sprawdzLiczbeOcen(EditText editText, String info)  {
+        int o;
+        if("".equals(editText.getText().toString())){
+            o = 0;
+        } else {
+            try {
+                o = Integer.parseInt(editText.getText().toString());
+            } catch (NumberFormatException e) {
+                o = 0;
+            }
+        }
+        if(o < 5 || o > 15) {
+            editText.setError(info);
+            Toast.makeText(this, info, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void sprawdzPotwierdzBtWidoczny() {
+        this.isPotwierdzEtWidoczny = this.isImieEtOk && this.isNazwiskoEtOk && this.isLiczbaOcenEtOk;
+        Button potwierdzBt = findViewById(R.id.potwierdz_bt);
+        potwierdzBt.setVisibility(this.isPotwierdzEtWidoczny ? View.VISIBLE : View.GONE);
     }
 }
