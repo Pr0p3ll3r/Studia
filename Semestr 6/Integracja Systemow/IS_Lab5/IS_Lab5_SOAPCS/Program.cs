316ee6ea3f7f3f05cf02cd060fc1aac551d08af5
Jakub Dudek <kuba395@gmail.com>
2023-04-13 16:35:21 +0200

    IS Lab5 SOAP

---
+++ Semestr 6/Integracja Systemow/IS_Lab5/IS_Lab5_SOAPCS/Program.cs
@@ -0,0 +1,22 @@
+﻿using ServiceReference1;
+
+namespace IS_Lab5_SOAPCS
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("My First SOAP Client!");
+            
+            MyFirstSOAPInterfaceClient client = new MyFirstSOAPInterfaceClient();
+            
+            string text = await
+            client.getHelloWorldAsStringAsync("Karol");
+            Console.WriteLine(text);
+            
+            long days = await
+            client.getDaysBetweenDatesAsync("10 04 2023", "12 04 2023");
+            Console.WriteLine(days);
+        }
+    }
+}
