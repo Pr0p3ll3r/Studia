9ce5bdd4b2e9920a19f89bf96e51e7fcdc7aa10f
Jakub Dudek <kuba395@gmail.com>
2023-11-22 20:02:23 +0100

    .NET Lab6 Userdata validation

---
+++ Semestr 7/Architektura i programowanie w .NET/Lab6/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tytuł filmu nie może być pusty")]
+        [MaxLength(50, ErrorMessage = "Tytuł filmu może mieć co najwyżej 50 znaków")]
+        public string Title { get; set; }
+        [UIHint("LongText")]
+        [Required(ErrorMessage = "Opis filmu nie może być pusty")]
+        public string Description { get; set; }
+        [UIHint("Stars")]
+        [Required(ErrorMessage = "Podaj ocenę filmu")]
+        [Range(1, 5, ErrorMessage = "Ocena filmu musi być liczbą pomiędzy 1 a 5")]
+        public int Rating { get; set; }
+        public string? TrailerLink { get; set;}
+        public Genre Genre { get; set; }
+    }
+}
